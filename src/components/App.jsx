21d1d84e5b0702914c6f18fb0b4bfbe96535fd4c
Justas Gražulevičius <91288874+jusgra@users.noreply.github.com>
2023-05-24 23:10:36 +0300

    Added some CSS, fixed a few bugs

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -6,20 +6,21 @@ function App() {
   const [inputState, setInput] = useState({ city: "", units: "metric" });
   const shouldEffect = useRef(false);
 
-  async function buttonClick() {
+  async function buttonClick(e) {
+    console.log("buttoned");
+    e.preventDefault();
     setResults({ temp: "", time: "", city: "" });
     const url = urlConstruct(inputState.city, inputState.units);
     let response = await fetch(url);
 
     if (response.ok) {
       response = await response.json();
-      //console.log(response);
       const resTemp = response.list[0].main.temp;
       const resCity = response.city.name;
       const resTimezone = response.city.timezone;
 
       const timeSearch = getLocalTime(response.city.timezone);
-      shouldEffect.current = !shouldEffect.current;
+      if (!shouldEffect.current) shouldEffect.current = !shouldEffect.current;
       setResults({
         temp: Math.floor(resTemp),
         time: timeSearch,
@@ -27,10 +28,6 @@ function App() {
         timezone: resTimezone,
       });
     } else console.log("Response ERROR");
-  }
-
-  function stop() {
-    shouldEffect.current = !shouldEffect.current;
   }
 
   useEffect(() => {
@@ -45,14 +42,6 @@ function App() {
         };
       });
     }, 1000);
-    // setTimeout(() => {
-    //   setResults((prevRes) => {
-    //     return {
-    //       ...prevRes,
-    //       time: getLocalTime(resultsState.timezone),
-    //     };
-    //   });
-    // }, 1000);
     return () => {
       clearInterval(int);
     };
@@ -70,34 +59,36 @@ function App() {
 
   return (
     <>
-      <input onChange={handleChange} value={inputState.city} type="text" name="city"></input>
-      <select onChange={handleChange} name="units">
-        <option value="metric">C</option>
-        <option value="imperial">F</option>
-      </select>
-      <button onClick={buttonClick}>Find</button>
-      <button onClick={stop}>ST</button>
+      {/* <div className="bg-image"></div> */}
+      <Results className="results" city={resultsState.city} temp={resultsState.temp} time={resultsState.time} />
 
-      <Results city={resultsState.city} temp={resultsState.temp} time={resultsState.time} />
+      <form onSubmit={buttonClick}>
+        <input
+          onChange={handleChange}
+          autocomplete="off"
+          value={inputState.city}
+          type="text"
+          name="city"
+          placeholder="City name"
+        ></input>
+        <div className="inputs">
+          <select onChange={handleChange} name="units">
+            <option value="metric">&deg;C</option>
+            <option value="imperial">&deg;F</option>
+          </select>
+          <button type="submit">Find</button>
+        </div>
+      </form>
     </>
   );
 }
 
 function getLocalTime(timezone) {
-  // const utc = new Date();
-  // const offset = timezone / 3600;
-  // const utcH = utc.getUTCHours() + offset;
-  // const utcM = utc.getUTCMinutes();
-  // const utcS = utc.getUTCSeconds();
-  // return utcH + ":" + utcM + ":" + utcS;
-
   const dateNow = new Date();
   const offSet = timezone / 3600;
-  //console.log(timezone / 3600);
-  //console.log("dN - " + dateNow);
   const localHours = dateNow.getUTCHours() + offSet;
   dateNow.setHours(localHours);
-  return dateNow.toLocaleTimeString();
+  return dateNow.toLocaleTimeString("en-GB");
 }
 
 function urlConstruct(city, unit) {
